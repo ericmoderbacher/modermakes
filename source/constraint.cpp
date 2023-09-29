160106405edacc9380a1503b83b92c673d484246
ericmoderbacher <eric@moderbacher.com>
2023-09-28 22:07:00 -0700

    compiles... doesn't do much else

--- source/constraint.cpp
+++ source/constraint.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "constraint.h"
+#include <cstdint>
+
+//kind of like an "is" constraint... might just be a place holder for now.
+constraint::constraint(uint64_t * subject)
+{
+    //set the predicate to a function that always returns true so long as the subject is not null.
+    this->predicate = [](uint64_t * subject) -> bool {return subject != nullptr;};
+
+    //set the subject to the subject.
+    this->subject = subject;
+}
--- source/point.h
+++ source/point.h
@@ -5,4 +5,24 @@
 #ifndef LUA_AIDED_DESIGN_POINT_H
 #define LUA_AIDED_DESIGN_POINT_H
 
+#include "constraint.h"
+
+class point
+{
+private:
+    uint64_t x;
+    uint64_t y;
+    uint64_t z;
+
+    constraint x_constraint;
+    constraint y_constraint;
+    constraint z_constraint;
+public:
+
+    point(uint64_t, uint64_t, uint64_t); //hopefully these will be simpler
+    point(constraint, constraint, constraint);
+
+    bool isConstrained();
+};
+
 #endif //LUA_AIDED_DESIGN_POINT_H
