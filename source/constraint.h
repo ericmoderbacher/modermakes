80321ca746a380d67037a542bd09f9d0a07da0f9
ericmoderbacher <eric@moderbacher.com>
2023-09-28 22:07:00 -0700

    compiles... doesn't do much else

--- source/constraint.h
+++ source/constraint.h
@@ -5,10 +5,18 @@
 #ifndef LUA_AIDED_DESIGN_CONSTRAINT_H
 #define LUA_AIDED_DESIGN_CONSTRAINT_H
 
+#include <cstdint>
 
 class constraint
 {
+private:
+    uint64_t * subject; //like of a sentence.
 
+    //function pointer to a function that returns bool if the constraint is met.
+    bool (*predicate)(uint64_t * subject);
+
+public:
+    constraint(uint64_t *);
 };
 
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,11 @@
 //
 // Created by Eric Moderbacher on 9/28/23.
 //
+#include <iostream>
 
 
 int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
