e7996791c36353eb1931ff49818042aa5dd4ff9f
ericmoderbacher <eric@moderbacher.com>
2025-05-05 19:42:31 -0700

    meh

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-//
-// Created by Eric Moderbacher on 1/16/24.
-//
 
 
 int main()
 {
-    //What if the goal was to create a die?
 
-    //we would create a cube by either defining the faces, or the points and their links
+
 
 
 
